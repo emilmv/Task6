9e87711c3aef55bb2d6e1a2c3c9766340b2a7689
emilmv <emilnm@code.edu.az>
2024-02-15 10:38:57 +0400

    Task 6 added

---
+++ Task6/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Hello, World!");
+#region Task1
+//Console.WriteLine("Shkafin enini daxil edin:");
+//int a=Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Shkafin hundurluyunu daxil edin:");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Qapinin enini daxil edin:");
+//int c = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Qapinin hundurluyunu daxil edin:");
+//int d = Convert.ToInt32(Console.ReadLine());
+//int length = c - a;
+//int height = d - b;
+//if (length > 0)
+//{
+//    if (height > 0)
+//    {
+//        Console.WriteLine("Cavab: Kececek");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Cavab: Kecmeyecek");
+//}
+#endregion
+#region Task2
+//Console.WriteLine("Eded daxil edin:");
+//int n = Convert.ToInt32(Console.ReadLine());
+//while (n > 1)
+//{
+//    n /= 2;
+//}
+//if (n == 1)
+//{
+//    Console.WriteLine("Cavab: 2-nin quvvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("2-nin quvveti deyil");
+//}
+#endregion
+#region Task3
+//Console.WriteLine("Eded daxil edin:");
+//int a = Convert.ToInt32(Console.ReadLine());
+//int result = 0;
+//while (a > 0)
+//{
+//    result++;
+//    a /= 10;
+//}
+//Console.WriteLine("Mertebelerin sayi: "+result);
+#endregion
